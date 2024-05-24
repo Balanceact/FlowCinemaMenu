aed7272b9af5aa404e02b7df7d3adf0c73b5b015
Balanceact <makildur@hotmail.com>
2024-05-24 09:11:48 +0200

    Add project files.

---
+++ FlowCinemaMenu/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace FlowCinemaMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = Menu();
+            }
+        }
+
+        private static bool Menu()
+        {
+            Console.Clear();
+            PrintMenu();
+            string menuChoice = Console.ReadLine();
+            switch (menuChoice)
+            {
+                case "0":
+                    return false;
+                    break;
+                default:
+                    Console.WriteLine("Please pick a valid choice.");
+                    return true;
+                    break;
+            }
+        }
+
+        private static void PrintMenu()
+        {   
+            Console.WriteLine("Welcome to your local cinema. Choose an option:");
+            Console.WriteLine("0) Quit / Exit");
+        }
+
+    }
+}
